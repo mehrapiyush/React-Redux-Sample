f0ae6c7e975fc305be5eaebae017087943662ca4
Piyush <mehrapiyush1271@gmail.com>
2020-04-05 17:39:37 +0530

    changes

---
+++ src/Components/ComponentOne/ComponentOne.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import allActions from "../../Actions";
+
+const ComponentOne = props => {
+  const counter = useSelector(state => state.counter);
+  const text = useSelector(state => state.text.value);
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="ComponentOne">
+      <h5>1. Fuctional Component</h5>
+
+      <p>Counter: {counter}</p>
+      <p>Text: {text} </p>
+
+      <button
+        className="btn btn-outline-primary"
+        onClick={() => dispatch(allActions.counterActions.increment())}
+      >
+        Increase Counter
+      </button>
+      <button
+        className="btn btn-outline-secondary"
+        onClick={() => dispatch(allActions.counterActions.decrement())}
+      >
+        Decrease Counter
+      </button>
+    </div>
+  );
+};
+
+export default ComponentOne;
